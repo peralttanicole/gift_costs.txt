036a81dd267054c7c75a5e9dc15d87f93a56789b
Nicole Peralta <119994410+peralttanicole@users.noreply.github.com>
2022-12-08 18:00:25 +0000

    Este es el paso 4

--- main.py
+++ main.py
@@ -1,29 +1,30 @@
+#Nicole A. Peralta
+#22-0262
+# Refinando codigos
+# En este programa lo que hicimos fue refinar un codigo con los estandares establecidos en la clase
+
 # Cree una funcion que me recibia la ruta del archivo de costos como entrada y me devolvia una lista con los costos en formato numérico.
 
-def ruta_archivo(): 
-  with open('gift_costs.txt') as f:
-    gift_costs = f.read().split('\n')
-  gift_costs = [int(c) for c in gift_costs]  # convierte strings a int
-  return gift_costs
-  
+def costs_list():
+    with open('gift_costs.txt', 'r', encoding='UTF-8') as archivo:
+        gift_costs = list(archivo)
+    gift_costs = [int(c) for c in gift_costs]  # convierte strings a int
+    archivo.close()  # cerrar el archivo después de usarlo y no ser necesario
+    return gift_costs
+
 # Cree una funcion que me recibio la lista de los costos como entrada y devuelva el precio total.
-  
-def devolver_precio(gift_costs):
-  gift_costs= ruta_archivo ()
-  total_price = 0
-  for cost in gift_costs:
-    if cost > 1000:
-      total_price += cost * 1.16  # agrega impuestos
-    else:
-       total_price += cost
-  return total_price
-  
+
+def total(gift_costs):
+    total_price = 0
+    for cost in gift_costs:
+        if cost > 1000:
+            total_price += cost * 1.16  # agrega impuestos
+        else:
+            total_price += cost  # los costos menores a 1000 no se le agrega impuesto
+
+    return total_price
+
 # Cree una funcion que me llamo a las anteriores para luego imprimir el precio de los regalos en pantalla.
   
-def principal():
-  gift_costs= ruta_archivo ()
-  total_price= devolver_precio (gift_costs)
-  print(total_price)
-
-if __name__ == '__main__':
-  principal()
+def main():
+    print(total(costs_list()))
