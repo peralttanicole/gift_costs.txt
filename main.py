061a1d5e8078610815bed6d3268534f1b2585bd1
Nicole Peralta <119994410+peralttanicole@users.noreply.github.com>
2022-12-08 20:42:36 +0000

    Esta es mi última actualización con el paso 7

--- main.py
+++ main.py
@@ -1,42 +1,35 @@
-#Nicole A. Peralta
-#22-0262
-# Refinando codigos
-# En este programa lo que hicimos fue refinar un codigo con los estandares establecidos en la clase
+'''Nicole A. Peralta
+22-0262
+Refinando codigos
+En este programa lo que hicimos fue refinar un codigo con los estandares establecidos en la clase'''
 
 import sys
 
-# Cree una funcion que me recibia la ruta del archivo de costos como entrada y me devolvia una lista con los costos en formato numérico.
-
 def lista_costo():
-    # Función que trae la lista de costos del archivo gift_costs.txt
-    with open('gift_costs.txt', 'r', encoding='UTF-8') as f:
-        gift_costs = list(f)
+    '''Función que trae la lista de costos del archivo gift_costs.txt'''
+    with open('gift_costs.txt', 'r', encoding='UTF-8') as archivo:
+        gift_costs = list(archivo)
         try:
             gift_costs = [int(c) for c in gift_costs]  # conversion de strings a int
-            f.close()  # cierra el archivo despues de ser ejecutado
+            archivo.close()  # cierra el archivo despues de ser ejecutado
         except ValueError:
             print('La información debe ser numerico.')
             sys.exit()
-
     return gift_costs
 
-
 def sum_tot(gift_costs):
-    # Función que suma los precios de la lista de costos 
+    '''Función que suma los precios de la lista de costos'''
     total_price = 0
     for costo in gift_costs:
         if costo > 1000:
             total_price += costo * 1.16  # calcular impuesto
         else:
             total_price += costo  # sin impuesto los costos menores de 1000
-
     return total_price
 
-
 def main():
-    # Función principal que invoca las funciones y gebera el costo total
+    '''Función principal que invoca las funciones y gebera el costo total'''
     return print(sum_tot(lista_costo()))
-
 
 if __name__ == '__main__':
     main()
